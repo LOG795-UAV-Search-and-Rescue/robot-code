9dc7d8c67b8a53b2c9339a4dc9a8af6b0f831330
Raphaël Camara <144168359+RaphaelCamara@users.noreply.github.com>
2025-10-01 13:11:51 -0400

    yeetus

--- drivers/ugv_driver.go
+++ drivers/ugv_driver.go
@@ -1,7 +1,7 @@
 package drivers
 
 import (
-	"encoding/json"
+	"fmt"
 	"log"
 	"sync"
 
@@ -10,43 +10,62 @@ import (
 
 var mu sync.Mutex
 
+/* UGVDriver is a driver for the UGV control board. */
 type UGVDriver struct {
 	Device string
 	Mode   serial.Mode
 }
 
-type CMD_SPD_CTRL struct {
-	T int     `json:"T"`
-	L float32 `json:"L"`
-	R float32 `json:"R"`
+/* Initialise the UGV control board. */
+func (d *UGVDriver) Init() error {
+	d.sendCommand([]byte(`{"T":142,"cmd":50}`))                           // set feedback interval
+	d.sendCommand([]byte(`{"T":131,"cmd":1}`))                            // serial feedback flow on
+	d.sendCommand([]byte(`{"T":143,"cmd":0}`))                            // serial echo off
+	d.sendCommand([]byte(`{"T":4,"cmd":0}`))                              // select the module - 0:None, 1:RoArm-M2-S, 2:Gimbal
+	d.sendCommand([]byte(`{"T":300,"mode":0,"mac":"EF:EF:EF:EF:EF:EF"}`)) // the base won't be ctrl by esp-now broadcast cmd, but it can still recv broadcast megs.
+	d.sendCommand([]byte(`{"T":900,"main":2,"module":0}`))                // set product version
+	return nil
 }
 
+/* Send command to the UGV control board in a JSON format. */
+func (d *UGVDriver) SendJSON(cmd string) error {
+	return d.sendCommand([]byte(cmd))
+}
+
+/* Set the target linear velocities for the left and right wheels in m/s. */
 func (d *UGVDriver) SetSpeed(left, right float32) error {
-	cmd := CMD_SPD_CTRL{
-		T: 1,
-		L: left,
-		R: right,
-	}
-
-	jsonData, err := json.Marshal(cmd)
-	if err != nil {
-		log.Println("Error marshalling to JSON: ", err)
-		return err
-	}
-
-	return d.sendCommand(jsonData)
+	cmd := fmt.Sprintf(`{"T":1,"L":%.2f,"R":%.2f}`, left, right)
+	return d.SendJSON(cmd)
 }
 
-func (d *UGVDriver) SendJSON(cmd string) {
-	log.Printf("Sent through \"%s\".\n", cmd)
-	d.sendCommand([]byte(cmd))
+/*
+Set the Power going to the wheels.
+
+left and right must be between -255 and 255, if out of bounds they are set to bound.
+*/
+func (d *UGVDriver) SetPowerToWheels(left, right int16) error {
+	if left > 255 {
+		left = 255
+	}
+	if left < -255 {
+		left = -255
+	}
+	if right > 255 {
+		right = 255
+	}
+	if right < -255 {
+		right = -255
+	}
+
+	cmd := fmt.Sprintf(`{"T":11,"L":%d,"R":%d}`, left, right)
+	return d.SendJSON(cmd)
 }
 
 func (d *UGVDriver) sendCommand(cmd []byte) error {
 	mu.Lock()
 	defer mu.Unlock()
 
-	log.Printf("Sending \"%s\".\n", cmd)
+	log.Printf("Sending %s.\n", cmd)
 
 	port, err := serial.Open(d.Device, &d.Mode)
 	if err != nil {
